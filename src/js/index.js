7a480bf71efd520f30948e038032d238784395f1
cobalt sato <cobaltsato@cobalts-iMac.local>
2019-08-17 13:58:27 +0900

    save point before refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 var contract;
 var userAccount;
-var contractAddress = "0x92Df726D3D5844afA62F2878Ce4C96BB6C307f03";
+var contractAddress = "0x1150aFBE434Db783fD62e0Bff2D74217f53EF262";
 
 async function startApp() {
     await web3.eth.getAccounts((error, accounts) => {
@@ -18,17 +18,29 @@ async function startApp() {
 
     contract = await new web3.eth.Contract(contractABI, contractAddress);
 
-await contract.events.NewZombie({
-    fromBlock: 0
-}, function(error, event){
-contract.methods.ownerOf(event.returnValues.zombieId).call().then((owner) => {
-    if(owner === userAccount)
- generateZombie(event.returnValues.zombieId, event.returnValues.name, event.returnValues.dna)
+    // Listen for the `NewZombie` event, and update the UI
+    await contract.events.NewZombie({
+        fromBlock: 0
+    }, function (error, event) {
+        contract.methods.ownerOf(event.returnValues.zombieId).call().then((owner) => {
+            if (owner === userAccount) {
+                document.getElementById('signUp').classList.add('hide');
+                document.getElementById('yourZombies').classList.remove('hide');
+                let ret = event.returnValues;
+                contract.methods.zombies(ret.zombieId).call().then((zombie) => {
+                    generateZombie(ret.zombieId, 
+                        ret.name, ret.dna, 
+                    'zombieList',
+                    zombie.readyTime,
+                    zombie.level);
+                });
+            }
         }, false);
-         //console.log(event);
-         })
-.on('error', console.error);
-
+        //console.log(event);
+    })
+        .on('error', console.error);
+    generateZombie(-1, "NO NAME", "0000000000000000", 'preview');
+    displayZombieDetailsOnPreview(calcZombieDetails(-1, "NO NAME", '0000000000000000'));
 }
 
 window.addEventListener('load', () => {
@@ -37,14 +49,11 @@ window.addEventListener('load', () => {
     } else {
         console.log('metamask not found');
     }
-    console.log('tes');
     startApp();
 })
 
-// some sort of event listener to take the text input:
-$("#ourButton").click(async function (e) {
+$("#btnCreateZombie").click(async function (e) {
     var name = $("#nameInput").val()
-    // Call our contract's `createRandomZombie` function:
     await contract.methods.createRandomZombie(name).send({ from: userAccount })
         .on("transactionHash", (txHash) => {
             alert("Txhash: " + txHash);
@@ -57,99 +66,62 @@ $("#ourButton").click(async function (e) {
         });
 })
 
-/*
-// Listen for the `NewZombie` event, and update the UI
-var event = ZombieOwnership.NewZombie(function(error, result) {
- if (error) return
- generateZombie(result.zombieId, result.name, result.dna)
-})
-*/
 
-// take the Zombie dna, and update our image
-function generateZombie(id, name, dna) {
-    // calc Zombie Details from dna
-    //=======================================================================
-    let dnaStr = String(dna)
-    // pad DNA with leading zeroes if it's less than 16 characters
-    while (dnaStr.length < 16)
-        dnaStr = "0" + dnaStr
 
-    let zombieDetails = {
-        // first 2 digits make up the head. We have 7 possible heads, so % 7
-        // to get a number 0 - 6, then add 1 to make it 1 - 7. Then we have 7
-        // image files named "head1.png" through "head7.png" we load based on
-        // this number:
-        headChoice: dnaStr.substring(0, 2) % 7 + 1,
-        // 2nd 2 digits make up the eyes, 11 variations:
-        eyeChoice: dnaStr.substring(2, 4) % 11 + 1,
-        // 6 variations of shirts:
-        shirtChoice: dnaStr.substring(4, 6) % 6 + 1,
-        // last 6 digits control color. Updated using CSS filter: hue-rotate
-        // which has 360 degrees:
-        skinColorChoice: parseInt(dnaStr.substring(6, 8) / 100 * 360),
-        eyeColorChoice: parseInt(dnaStr.substring(8, 10) / 100 * 360),
-        clothesColorChoice: parseInt(dnaStr.substring(10, 12) / 100 * 360),
-        hadhadCat: (parseInt(dnaStr) % 100 === 99) ,
-        zombieName: name,
-        zombieDescription: "A Level 1 CryptoZombie",
-    }
+function onReadyTime(strTime){
+    intTime = parseInt(strTime);
+    var date = new Date();
+    if(date.getTime() > intTime)
+        return " Yes";
+    var d = new Date( intTime );
+    var year  = d.getFullYear();
+    var d = new Date( intTime );
+    var y = new Date( intTime * 1000 );
+    var year  = y.getFullYear();
+    var month = d.getMonth() + 1;
+    var day  = d.getDate();
+    var hour = ( '0' + d.getHours() ).slice(-2);
+    var min  = ( '0' + d.getMinutes() ).slice(-2);
+    var sec   = ( '0' + d.getSeconds() ).slice(-2);
 
-    // create Zombie from zombieDetails
-    //=======================================================================
-
-    // clone from model
-    let model = document.getElementById('model');
-    model.classList.remove('hide'); 
-    let newZombie = model.cloneNode(true);
-
-    for (let i = 1; i <= 7; ++i) { // for 7 head parts
-        let headPart = newZombie.getElementsByClassName('head-part-' + i)[0];
-        if (i === zombieDetails.headChoice) {
-            headPart.classList.remove('hide');
-        }
-        else {
-            headPart.classList.add('hide');
-        }
-    }
-    for (let i = 1; i <= 11; ++i) { // for 11 eye parts
-        let eyePart = newZombie.getElementsByClassName('eye-part-' + i)[0];
-        if (i === zombieDetails.eyeChoice) {
-            eyePart.classList.remove('hide');
-        }
-        else {
-            eyePart.classList.add('hide');
-        }
-    }
-    for (let i = 1; i <= 6; ++i) { // for 6 shirt parts
-        let shirtPart = newZombie.getElementsByClassName('shirt-part-' + i)[0];
-        if (i === zombieDetails.shirtChoice) {
-            shirtPart.classList.remove('hide');
-        }
-        else {
-            shirtPart.classList.add('hide');
-        }
-    }
-    let skins = newZombie.getElementsByClassName('skin');
-    for (let i = 0; i < skins.length; ++i)
-        skins[i].setAttribute("style", "filter:hue-rotate(" + zombieDetails.skinColorChoice + "deg)");
-
-    let eyes = newZombie.getElementsByClassName('eye');
-    for (let i = 0; i < eyes.length; ++i)
-        eyes[i].setAttribute("style", "filter:hue-rotate(" + zombieDetails.eyeColorChoice + "deg)");
-
-    let clothes = newZombie.getElementsByClassName('cloth');
-    for (let i = 0; i < clothes.length; ++i)
-        clothes[i].setAttribute("style", "filter:hue-rotate(" + zombieDetails.clothesColorChoice + "deg)");
-
-        if(zombieDetails.hadhadCat){
-            newZombie.getElementsByClassName('cat-legs')[0].classList.remove('hide');
-            let target = newZombie.getElementsByClassName('legs');
-            for(let i = 0; i < target.length; ++i) target[i].classList.add('hide');
-        }
-    document.getElementById('zombieList').appendChild(newZombie);
-    model.classList.add('hide'); // model no longer needed
-    return zombieDetails
+    return( year + '/' + month + '/' + day + ' ' + hour + ':' + min + ':' + sec );
 }
 
+// detect name input at real time 
+// then, display the preview of the named zombie  
+//============================================================
+$(function () {
+    var $input = $('#nameInput');
+    $input.on('input', function (event) {
+        var name = $input.val();
+        // get DNA from SC
+        contract.methods.generateRandomDna(name).call().then((dna) => {
+            var preview = document.getElementById('preview');
+            while (preview.firstChild) preview.removeChild(preview.firstChild);
+
+            displayZombieDetailsOnPreview(calcZombieDetails(-1, name, dna));
+            generateZombie(-1, name, dna, 'preview');
+        }, false);
+
+    });
+});
+
+function displayZombieDetailsOnPreview(zombieDetails) {
+    document.getElementById('dna').textContent =
+        "頭部の遺伝子: " + zombieDetails.headChoice;
+    document.getElementById('eyes').textContent =
+        "目の遺伝子: " + zombieDetails.eyeChoice;
+    document.getElementById('shirts').textContent =
+        "シャツの遺伝子: " + zombieDetails.shirtChoice;
+    document.getElementById('skinColor').textContent =
+        "肌の色の遺伝子: " + zombieDetails.skinColorChoice;
+    document.getElementById('eyesColor').textContent =
+        "目の色の遺伝子: " + zombieDetails.eyeColorChoice;
+    document.getElementById('clothesColor').textContent =
+        "服の色の遺伝子: " + zombieDetails.clothesColorChoice;
+    
+}
+
+// TEST
 //generateZombie(1, "a", "2053823161706508");
 //generateZombie(1, "a", "4871286036524799");
